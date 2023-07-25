a6ca788ec433ad599cc58039de42f0fe324616dc
wxl <1715142548@qq.com>
2023-07-26 02:26:40 +0800

    fix: 非admin身份将无法创建/修改猪价

--- src/main/java/com/ps/controller/TodayPriceController.java
+++ src/main/java/com/ps/controller/TodayPriceController.java
@@ -4,13 +4,13 @@ import com.ps.pojo.Days;
 import com.ps.pojo.Result;
 import com.ps.pojo.Today_Price;
 import com.ps.service.TodayPriceService;
+import com.ps.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -27,7 +27,12 @@ public class TodayPriceController {
     }
 
     @PostMapping("/createTodayPrice")
-    public Result createTodayPrice(@RequestBody Today_Price todayPrice){
+    public Result createTodayPrice(@RequestHeader String Authorization,@RequestBody Today_Price todayPrice){
+        Claims claims = JwtUtils.parseJWT(Authorization);
+        String user_auth = (String) claims.get("user_auth");
+        if(!Objects.equals(user_auth, "admin")) {
+            return Result.error("无平台方权限");
+        }
         Today_Price todayPrice1 = todayPriceService.createTodayPrice(todayPrice.getToday_price(),todayPrice.getToday_price_time());
         return Result.success(todayPrice1);
     }
